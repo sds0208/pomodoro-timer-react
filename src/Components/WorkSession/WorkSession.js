cb51665f8ef8d95a3052cf397daf1c7c9483967c
sds0208 <sarahsantacruz86@gmail.com>
2018-02-03 12:46:23 -0600

    work session timer component added; timer counts down from 25 minutes, displays time properly, starts on click of start button, resets on click of reset button

--- src/Components/WorkSession/WorkSession.js
+++ src/Components/WorkSession/WorkSession.js
@@ -6,23 +6,40 @@ class WorkSession extends Component {
     this.state = { workTime: 1500 };
   }
 
-  componentDidMount() {
-    this.countdown();
+  componentWillUpdate(nextProps, nextState) {
+    if (this.state.workTime === 1) {
+      this.stopCountdown();
+    }
   }
 
-  componentWillUnmount() {
-
+  countdown(workTime) {
+    if (this.state.workTime > 0) {
+      this.interval = setInterval(() => this.setState({ workTime: this.state.workTime - 1 }), 1000);
+    }
   }
 
-  countdown() {
-    setInterval(() => this.setState({ workTime: this.state.workTime - 1 }), 1000);
+  stopCountdown() {
+    clearInterval(this.interval);
+  }
+
+  resetCountdown() {
+    this.stopCountdown();
+    this.setState({ workTime: 1500 });
+  }
+
+  minutesAndSeconds(rawSeconds) {
+    const seconds = rawSeconds % 60;
+    const minutes = Math.floor(rawSeconds / 60);
+    return seconds < 10 ? minutes + ':0' + seconds : minutes + ':' + seconds;
   }
 
   render() {
     return(
       <div className="work-session">
-        <div className="work-timer">{this.state.workTime}</div>
-        <button className="start-stop-button">Start/Stop</button>
+        <h3>Work Session</h3>
+        <div className="work-timer">{this.minutesAndSeconds(this.state.workTime)}</div>
+        <button className={this.state.workTime >= 1500 ? 'start-stop-button': 'no-show'} onClick={() => this.countdown(this.state.workTime)}>Start</button>
+        <button className={this.state.workTime < 1500 ? 'start-stop-button' : 'no-show'} onClick={() => this.resetCountdown()}>Reset</button>
       </div>
     );
   }
