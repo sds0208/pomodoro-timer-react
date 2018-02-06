6a787b0d5a8a8176ec342a27f6d31f6953faa4e4
sds0208 <sarahsantacruz86@gmail.com>
2018-02-05 20:53:23 -0600

    in progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 import Timer from './Components/Timer/Timer.js';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDmijLWiDm49W-pvRtWUamTfxT7wrTj1nA",
+  authDomain: "bloctime-8025f.firebaseapp.com",
+  databaseURL: "https://bloctime-8025f.firebaseio.com",
+  projectId: "bloctime-8025f",
+  storageBucket: "bloctime-8025f.appspot.com",
+  messagingSenderId: "306578503486"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
